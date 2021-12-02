c5874f7d166364de07aa76c5f139341370587fbd
ria-leberu <gene.andrei.nadela@gmail.com>
2021-12-01 21:25:39 -0500

    added heap notes

---
+++ heaps/notes_heaps.py
@@ -0,0 +1,11 @@
+# Heaps are a tree data structure that satisfy the heap property:
+# Min Heaps are when the parent node is less than or equal to a child, the minimum value at the root
+# Max Heaps are when the parent is greater than or equal to a child, the maximum value at the root
+# Binary heaps are the most common
+# A priority queue is an abstract data type, and a heap is the implementation of it
+
+# Heaps are used for finding the smallest or largest item in a dataset quickly
+# When you see problems like "Kth" in the title, consider heaps
+
+# Important areas where heaps are used:
+# Heap sort, Graph algorithms (to find the shortest path, like Djikstras or Prim's MST), Selection algorithms (find median in data stream, find kth element in a list), K-way merge (merging data sets)
